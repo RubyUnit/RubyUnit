ea77e5c8d5fb0739d59e6451a3d0095c2c8de210
Matthew Clower <matthew.clower@wepay.com>
2014-10-04 21:23:07 -0700

    added sample class for testing

---
+++ sample/RealNumber.rb
@@ -0,0 +1,27 @@
+# Real Numbers (not complex)
+class RealNumber
+  # determines if a number is greater than zero
+  def positive?
+    self > 0
+  end
+
+  # determines if a number is less than zero
+  def negative?
+    self < 0
+  end
+end
+
+class Integer
+  # mixin RealNumber into Integer
+  include RealNumber
+end
+
+class Float
+  # mixin RealNumber into Float
+  include RealNumber
+end
+
+class Rational
+  # mixin RealNumber into Rational
+  include RealNumber
+end
