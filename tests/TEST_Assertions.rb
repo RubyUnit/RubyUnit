c148c067b6eb7531247f677c66c33ca85a62c79c
Matthew Clower <matthew.clower@wepay.com>
2014-10-16 21:44:55 -0700

    removing method tests... they are redundant in this class and getting rid of Fixture

--- tests/TEST_Assertions.rb
+++ tests/TEST_Assertions.rb
@@ -3,20 +3,8 @@ require 'RubyUnit/Assertions'
 # Data provider for RubyUnit::TestCase tests
 require_relative 'data/Assertions'
 
-# Fixture for RubyUnit::TestCase tests
-require_relative 'fixture/Assertions'
-
 #
 # Test Case for RubyUnit::Assertions module
 #
 class TEST_Assertions < RubyUnit::TestCase
-  include AssertionsData
-
-  #
-  # Test that the correct instance methods are defined
-  #
-  def instanceMethodTest method
-    asserts = AssertionsFixture.new
-    assertRespondTo asserts, method, 'Assertions object missing instance method'
-  end
 end
--- tests/fixture/Assertions.rb
+++
@@ -1,7 +0,0 @@
-#
-# Fixture for RubyUnit::Assertions tests
-#
-class AssertionsFixture
-  include RubyUnit::Assertions
-  # This is a fixture, NO TESTS
-end
