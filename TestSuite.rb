5b784c9044893a1d46eb25b1787874b224796e68
Matthew Clower <matthew.clower@wepay.com>
2014-10-17 22:34:52 -0700

    Loading all test sets automatically

--- TestSuite.rb
+++ TestSuite.rb
@@ -5,29 +5,10 @@
 path = File.dirname(__FILE__)
 
 $LOAD_PATH.unshift("#{path}/lib") if not ARGV.include? '--gem'
-$LOAD_PATH.unshift("#{path}/tests")
 
 require 'RubyUnit'
 
-# Test Sets
-
-# RubyUnit module => RubyUnit.rb
-require 'TS_RubyUnit'
-
-# AssertionFailure exception => RubyUnit/AssertionFailure.rb
-require 'TS_AssertionFailure'
-
-# Assertions module => RubyUnit/Assertions.rb
-require 'TS_Assertions'
-
-# IncompleteTest exception => RubyUnit/IncompleteTest.rb
-require 'TS_IncompleteTest'
-
-# Runner class => RubyUnit/Runner.rb
-require 'TS_Runner'
-
-# SkippedTest exception => RubyUnit/SkippedTest.rb
-require 'TS_SkippedTest'
-
-# TestCase class => RubyUnit/TestCase.rb
-require 'TS_TestCase'
+# Automatically load Test Sets
+Dir['tests/TS_*.rb'].each do |test_case|
+  require_relative test_case
+end
