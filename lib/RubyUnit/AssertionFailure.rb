bbb21707113fcee593b06337bdd15a383fd7a674
Matthew Clower <matthew.clower@wepay.com>
2014-10-17 19:49:39 -0700

    adding initialize to AssertionFailure so it can take data

--- lib/RubyUnit/AssertionFailure.rb
+++ lib/RubyUnit/AssertionFailure.rb
@@ -3,11 +3,17 @@ module RubyUnit
   # Exception that is raised when a test assertion fails.
   #
   class AssertionFailure < StandardError
+    attr_reader :data
+
     # Error messages
     FAILING = 'Failing test'
 
     def initialize data = {}, message
       raise ArgumentError, 'Data for AssertionFailure must be a Hash' unless data.is_a? Hash
+      @data = data
+    end
+
+    def info
     end
   end
 end
