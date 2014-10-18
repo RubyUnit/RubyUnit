a52765389c16a3446f01ffd9ded5d55de2997c8b
Matthew Clower <matthew.clower@wepay.com>
2014-10-18 16:40:02 -0700

    adding class assertion constant tests

--- tests/AssertionMessage/data/Constant.rb
+++ tests/AssertionMessage/data/Constant.rb
@@ -5,6 +5,7 @@ module AssertionMessageTests
   module ConstantData
     def constantData
       [
+        ##
         # Baisc assertion messages
         [             'FAILING'],
         [        'ASSERT_ERROR'],
@@ -13,6 +14,23 @@ module AssertionMessageTests
         [  'ASSERT_FALSE_ERROR'],
         [    'ASSERT_NIL_ERROR'],
         ['ASSERT_NOT_NIL_ERROR'],
+
+        ##
+        # Class assertion messages
+        [          'ASSERT_KIND_OF_ERROR'],
+        [      'ASSERT_NOT_KIND_OF_ERROR'],
+        [      'ASSERT_INSTANCE_OF_ERROR'],
+        [  'ASSERT_NOT_INSTANCE_OF_ERROR'],
+        [       'ASSERT_DESCENDENT_ERROR'],
+        [   'ASSERT_NOT_DESCENDENT_ERROR'],
+        [            'ASSERT_CONST_ERROR'],
+        [    'ASSERT_CONST_DEFINED_ERROR'],
+        ['ASSERT_CONST_NOT_DEFINED_ERROR'],
+
+        ##
+        # Collection assertion messages
+
+        ##
         # Comparison assertion messages
         [             'ASSERT_EQUAL_ERROR'],
         [         'ASSERT_NOT_EQUAL_ERROR'],
@@ -24,6 +42,12 @@ module AssertionMessageTests
         [         'ASSERT_NOT_MATCH_ERROR'],
         [              'ASSERT_SAME_ERROR'],
         [          'ASSERT_NOT_SAME_ERROR'],
+
+        ##
+        # Exception assertion messages
+
+        ##
+        # Method assertion messages
       ]
     end
   end
