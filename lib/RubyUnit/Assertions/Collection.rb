af5806019406d3bae6257eeb3999cd27513722e4
Matthew Clower <matthew.clower@wepay.com>
2014-10-20 01:02:35 -0700

    added helpers to simplify empty? and include? assertions

--- lib/RubyUnit/Assertions/Collection.rb
+++ lib/RubyUnit/Assertions/Collection.rb
@@ -17,8 +17,9 @@ module RubyUnit
     #  assertInclude [1, 2], 'not in', 'It does not, so... no'  # => fail
     #
     def assertInclude collection, value, message = nil
-      __validate_responds [:include?], collection
-      __assert (collection.include? value), ASSERT_INCLUDE_ERROR, message, {:collection=>collection, :value=>value}
+      __assert_include ASSERT_INCLUDE_ERROR, collection, value, message do
+        collection.include? value
+      end
     end
 
     #
@@ -37,8 +38,9 @@ module RubyUnit
     #  assertNotInclude [1, 2, 3], 2, 'It does, so close'  # => fail
     #
     def assertNotInclude collection, reject, message = nil
-      __validate_responds [:include?], collection
-      __reject (collection.include? reject), ASSERT_NOT_INCLUDE_ERROR, message, {:collection=>collection, :reject=>reject}
+      __assert_include ASSERT_NOT_INCLUDE_ERROR, collection, reject, message do
+        not collection.include? reject
+      end
     end
 
     #
@@ -55,8 +57,9 @@ module RubyUnit
     #  assertEmpty [1, 2], 'Not empty'  # => fail
     #
     def assertEmpty object, message = nil
-      __validate_responds [:empty?], collection
-      __assert object.empty?, ASSERT_EMPTY_ERROR, message, {:object=>object}
+      __assert_empty ASSERT_EMPTY_ERROR, object, message do
+        object.empty?
+      end
     end
 
     #
@@ -73,8 +76,24 @@ module RubyUnit
     #  assertNotInclude [1, 2, 3], 2, 'It does, so close'  # => fail
     #
     def assertNotEmpty object, message = nil
-      __validate_responds [:empty?], collection
-      __reject object.empty?, ASSERT_NOT_EMPTY_ERROR, message, {:object=>object}
+      __assert_empty ASSERT_EMPTY_ERROR, object, message do
+        not object.empty?
+      end
+    end
+
+    private
+    def __assert_include error, collection, value, message
+      raise ArgumentError, "#{collection.class} does not respond to :include?" unless collection.respond_to? :include?
+      __assert_block error, message, {:collection=>collection, :value=>value} do
+        yield
+      end
+    end
+
+    def __assert_empty error, object, message
+      raise ArgumentError, "#{object.class} does not respond to :empty?" unless collection.respond_to? :empty?
+      __assert_block error, message, {:object=>object} do
+        yield
+      end
     end
   end
 end
