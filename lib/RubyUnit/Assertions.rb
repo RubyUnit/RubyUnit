6435784166ca84403a0bb2e545bdede13921416c
Matthew Clower <matthew.clower@wepay.com>
2014-10-20 21:36:41 -0700

    subclassing the exceptions

--- lib/RubyUnit/Assertions.rb
+++ lib/RubyUnit/Assertions.rb
@@ -1,126 +1,32 @@
+require_relative 'Assertions/Basic'       # Basic assertions
+require_relative 'Assertions/Classes'     # Class assertions
+require_relative 'Assertions/Collections' # Collection assertions
+require_relative 'Assertions/Comparisons' # Comparison assertions
+require_relative 'Assertions/Exceptions'  # Exception assertions
+require_relative 'Assertions/Methods'     # Method assertions
+
 module RubyUnit
-  #
+  ##
   # Assertions that can be used by RubyUnit::TestCase
   #
   module Assertions
-    include AssertionMessage
-
+    ##
     # Tracks the total number of assertions made during the tests
     @@assertions = 0
 
-    private
-    #
-    # Builds the message that will be used with the assertion
-    # * raises RubyUnit::AssertionFailure
-    # * raises ArgumentError unless error is a String
-    # * raises ArgumentError unless message is nil or a String
-    # * raises ArgumentError unless data is a Hash
-    #
-    # error::
-    #   The assertion description
-    #
-    # message::
-    #   The message provided by the test for the assertion
-    #
-    # data::
-    #   The data associated with assertion failure
-    #
-    #  __fail 'Failing Test', message, {'expected' => expected, 'actual' => actual }
-    #
-    def __fail error, message, data = {} # :nodoc:
-      raise ArgumentError, 'Error description must be a String' unless error.is_a? String
-      raise ArgumentError, 'Failure message must be String' unless message.nil? or message.is_a? String
-      raise ArgumentError, 'Failure data must be a Hash' unless data.is_a? Hash
-      raise AssertionFailure.new({'Assertion Failure'=>message}.merge data), error
+    def self.assertions
+      @@assertions
     end
 
-    #
-    # The assertion wrapper is responsible for doing everything that must be done
-    # on each assertion.
-    # * keep track of the total number of assertions
-    #
-    # &block::
-    #   The assertion which is being wrapped
-    #
-    def __assert_block error = FAILING, message = nil, data = {} # :nodoc:
+    def self.add_assertion
       @@assertions += 1
-      result = block_given? ? yield : false
-      __fail error, message, data unless result
-      result
     end
 
-    #
-    # This is now a wrapper for __assert_block so it can be called 'nicely' in one line
-    # * raises RubyUnit::AssertionFailure unless _value_ is true 
-    #
-    # value::
-    #   The value to be asserted
-    #
-    # error::
-    #   The error associated with the assertion being checked
-    #
-    # message::
-    #   The message provided to be reported for a failure
-    #
-    # data::
-    #   The data associated with assertion
-    #
-    #  __assert value, 'Failed to assert value is true', message, {:value=>value}
-    #
-    def __assert value, error, message, data # :nodoc:
-      __assert_block error, message, data do
-        # this will result in returning true if an Exception is not raised
-        value
-      end
-    end
-
-    #
-    # Internally validate that an assertion is false or nil
-    # * raises RubyUnit::AssertionFailure unless _value_ is false or nil 
-    #
-    # value::
-    #   The value to be asserted
-    #
-    # error::
-    #   The error associated with the assertion being checked
-    #
-    # message::
-    #   The message provided to be reported for a failure
-    #
-    # data::
-    #   The data associated with assertion
-    #
-    #  __reject value, 'Failed to assert value is not true', message, {:value=>value}
-    #
-    def __reject value, error, message, data # :nodoc:
-      __assert !value, error, message, data
-    end
-
-    #
-    # Validate the parameters for exception assertions
-    # * raises ArgumentError if _pattern_ is not a String or Regexp
-    # * raises ArgumentError unless _e_ is a descendent of the Exception class
-    #
-    def __validate_exception pattern, e = Exception # :nodoc:
-      raise ArgumentError, 'Exception message must be a Regexp or String' unless pattern.is_a? Regexp or pattern.is_a? String
-      raise ArgumentError, 'Exception must be a subclass of Exception' unless e < Exception
-    end
-
-    #
-    # Validate an object accepts the correct methods for an assertion.
-    # * raises ArgumentError if a _object_ is not an instance of the corresponding _klass_
-    #
-    def __validate_responds methods, object #:nodoc:
-      methods.each do |method|
-        raise ArgumentError, "Object #{object.class} does NOT respond to #{method}" unless object.respond_to? method
-      end
-    end
+    include Assertions::Basic
+    include Assertions::Classes
+    include Assertions::Collections
+    include Assertions::Comparisons
+    include Assertions::Exceptions
+    include Assertions::Methods
   end
 end
-
-require_relative 'Assertions/Basic'      # Basic assertions
-require_relative 'Assertions/Class'      # Class assertions
-require_relative 'Assertions/Collection' # Collection assertions
-require_relative 'Assertions/Comparison' # Comparison assertions
-require_relative 'Assertions/Exception'  # Exception assertions
-require_relative 'Assertions/Method'     # Method assertions
---
+++ lib/RubyUnit/Assertions/Classes.rb
@@ -0,0 +1,216 @@
+require_relative 'Root'
+
+module RubyUnit
+  module Assertions
+    module Classes
+      include RubyUnit::AssertionMessage
+      include Root
+
+      ##
+      # Assert that an object is an instance of the specified class or one of
+      # its descendents.
+      # * raises RubyUnit::AssertionFailure unless _object_ is an instance of
+      #   _klass_ or one of its descendents. 
+      #
+      # klass::
+      #   The class that is expected
+      #
+      # object::
+      #   The object that will be checked against _klass_
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertKindOf String, 25, 'Nope, try again.'  # => fail
+      #
+      def assertKindOf klass, object, message = nil
+        __assert (object.is_a? klass), ASSERT_KIND_OF_ERROR, message, {:klass=>klass, :object=>object}
+      end
+    
+      alias_method :assertIsA, :assertKindOf
+
+      #
+      # Assert that an object is not an instance of the specified class or one of
+      # its descendents.
+      # * raises RubyUnit::AssertionFailure if _object_ is an instance of _exclusion_ or
+      # one of its descendents. 
+      #
+      # exclusion::
+      #   The class that is excluded
+      #
+      # object::
+      #   The object that will be checked against _klass_
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertNotKindOf Numeric, 25, 'Nope, try again.'  # => fail
+      #
+      def assertNotKindOf exclusion, object, message = nil
+        __reject (object.is_a? exclusion), ASSERT_NOT_KIND_OF_ERROR, message, {:exclusion=>exclusion, :object=>object}
+      end
+
+      [:assertNotIsA, :assertIsNotA].each do |method|
+        alias_method method, :assertNotKindOf
+      end
+
+      #
+      # Assert that an object is an instance of a specified class
+      # * raises RubyUnit::AssertionFailure unless _object_ is an instance of _klass_.
+      #
+      # klass::
+      #   The class that is expected
+      #
+      # object::
+      #   The object that will be checked against _klass_
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertInstanceOf Integer, '25', 'So close, but... No.'  # => fail
+      #
+      def assertInstanceOf klass, object, message = nil
+        __assert (object.instance_of? klass), ASSERT_INSTANCE_OF_ERROR, message, {:klass=>klass, :object=>object}
+      end
+
+      #
+      # Assert that an object is an instance of a specified class
+      # * raises RubyUnit::AssertionFailure unless _object_ is an instance of _klass_.
+      #
+      # exclusion::
+      #   The class that is expected
+      #
+      # object::
+      #   The object that will be checked against _klass_
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertNotInstanceOf Integer, 25, 'So close, but... No.'  # => fail
+      #
+      def assertNotInstanceOf exclusion, object, message = nil
+        __reject (object.instance_of? exclusion), ASSERT_NOT_INSTANCE_OF_ERROR, message, {:exclusion=>exclusion, :object=>object}
+      end
+
+      #
+      # Assert that a class is a descendent of another class
+      # * raises RubyUnit::AssertionFailure unless _descendent_ is a descendent of +_super+
+      #
+      # _super::
+      #   The parent class
+      #
+      # descendent::
+      #   The descendent class
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertDescendent Numeric, Exception, 'Nope'  # => fail
+      #
+      def assertDescendent _super, descendent, message = nil
+        __assert_descendent ASSERT_DESCENDENT_ERROR, _super, descendent, message do
+          descendent < _super
+        end
+      end
+
+      #
+      # Assert that a class is not a descendent of another class
+      # * raises RubyUnit::AssertionFailure if _klass_ is a descendent of _klass_
+      #
+      # klass::
+      #   The parent class
+      #
+      # descendent::
+      #   The illegal descendent class
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertDescendent StandardError, Exception, 'It is'  # => fail
+      #
+      def assertNotDescendent klass, descendent, message = nil
+        __assert_descendent ASSERT_NOT_DESCENDENT_ERROR, klass, descendent, message do
+          not descendent < klass
+        end
+      end
+
+      #
+      # Assert that a constant is defined correctly in the correct class
+      # * raises RubyUnit::AssertionFailure unless the constant is defined in
+      #   the specified class and it is the correct type and value
+      #
+      # expected::
+      #   The value that is expected for the constant
+      #
+      # klass::
+      #   The class where the constant should be defined
+      #
+      # konstant::
+      #   The name of the constant
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertConst 42, Numbers, 'TWENTYFOUR', 'So dyslexic.'  # => fail
+      #
+      def assertConst expected, klass, konstant, message = nil
+        __assert_block ASSERT_CONST_ERROR, message do
+          assertConstDefined klass, konstant, message
+          value = klass.const_get konstant
+          assertKindOf expected.class, value, message
+          assertEqual expected, value, message
+        end
+      end
+
+      #
+      # Assert that a constant is defined in the specified class
+      # * raises RubyUnit::AssertionFailure unless the constant is defined in
+      #   the specified class
+      #
+      # klass::
+      #   The class where the constant should be defined
+      #
+      # konstant::
+      #   The name of the constant
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertConstDefined Numbers, 'FORTYTWO', 'Mystery.'  # => ??
+      #
+      def assertConstDefined klass, konstant, message = nil
+        __assert (klass.const_defined? konstant), ASSERT_CONST_DEFINED_ERROR, message, {:klass=>klass, :konstant=>konstant}
+      end
+
+      #
+      # Assert that a constant is not defined in the specified class
+      # * raises RubyUnit::AssertionFailure if the constant is defined in
+      #   the specified class
+      #
+      # klass::
+      #   The class where the constant should not be defined
+      #
+      # konstant::
+      #   The name of the constant
+      #
+      # message::
+      #   The message provided to be reported for a failure
+      #
+      #  assertConstNotDefined Numbers, 'TWENTYFOUR', 'Mystery.'  # => ??
+      #
+      def assertConstNotDefined klass, konstant, message = nil
+        __reject (klass.const_defined? konstant), ASSERT_CONST_NOT_DEFINED_ERROR, message, {:klass=>klass, :konstant=>konstant}
+      end
+
+      private
+      ##
+      # Helper for asserting descendents
+      def __assert_descendent error, klass, descendent, message
+        raise ArgumentError, "Expecting Class, got #{klass.class}" unless klass.is_a? Class
+        __assert_block error, message, {:class=>klass, :descendent=>descendent} do
+          yield
+        end
+      end
+    end
+  end
+end
