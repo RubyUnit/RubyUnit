8e685ee038540b9d48d9034bd147779da9e7d9b4
Matthew Clower <matthew.clower@wepay.com>
2014-10-23 18:59:29 -0700

    added basic tests without content -- test plan for Comparison assertions

--- tests/Assertions/TC_Comparisons.rb
+++ tests/Assertions/TC_Comparisons.rb
@@ -1,6 +1,6 @@
 require 'RubyUnit/Assertions'
 
-# Data provider for RubyUnit::TestCase tests
+# Data provider for RubyUnit::Assertions Comparisons tests
 require_relative 'data/Comparisons'
 
 module AssertionsTests
@@ -9,5 +9,125 @@ module AssertionsTests
   #
   class TC_Comparisons < AssertionsTestCase
     include ComparisonsData
+
+    def assertEqualTest expected, value
+    end
+
+    def assertEqualFailTest expected, value
+    end
+
+    def assertEqualWithMessageTest expected, value, message
+    end
+
+    def assertEqualWithMessageFailTest expected, value, message
+    end
+
+    def assertNotEqualTest expected, value
+    end
+
+    def assertNotEqualFailTest expected, value
+    end
+
+    def assertNotEqualWithMessageTest expected, value, message
+    end
+
+    def assertNotEqualWithMessageFailTest expected, value, message
+    end
+
+    def assertGreaterThanTest expected, value
+    end
+
+    def assertGreaterThanFailTest expected, value
+    end
+
+    def assertGreaterThanWithMessageTest expected, value, message
+    end
+
+    def assertGreaterThanWithMessageFailTest expected, value, message
+    end
+
+    def assertGreaterThanOrEqualTest expected, value
+    end
+
+    def assertGreaterThanOrEqualFailTest expected, value
+    end
+
+    def assertGreaterOrThanEqualWithMessageTest expected, value, message
+    end
+
+    def assertGreaterThanOrEqualWithMessageFailTest expected, value, message
+    end
+
+    def assertLessThanTest expected, value
+    end
+
+    def assertLessThanFailTest expected, value
+    end
+
+    def assertLessThanWithMessageTest expected, value, message
+    end
+
+    def assertLessThanWithMessageFailTest expected, value, message
+    end
+
+    def assertLessThanOrEqualTest expected, value
+    end
+
+    def assertLessThanOrEqualFailTest expected, value
+    end
+
+    def assertLessThanOrEqualWithMessageTest expected, value, message
+    end
+
+    def assertLessThanOrEqualWithMessageFailTest expected, value, message
+    end
+
+    def assertMatchTest pattern, value
+    end
+
+    def assertMatchFailTest pattern, value
+    end
+
+    def assertMatchWithMessageTest pattern, value, message
+    end
+
+    def assertMatchWithMessageFailTest pattern, value, message
+    end
+
+    def assertNotMatchTest pattern, value
+    end
+
+    def assertNotMatchFailTest pattern, value
+    end
+
+    def assertNotMatchWithMessageTest pattern, value, message
+    end
+
+    def assertNotMatchWithMessageFailTest pattern, value, message
+    end
+
+    def assertSameTest expected, object
+    end
+
+    def assertSameFailTest expected, object
+    end
+
+    def assertSameWithMessageTest expected, object, message
+    end
+
+    def assertSameWithMessageFailTest expected, object, message
+    end
+
+    def assertNotSameTest expected, object
+    end
+
+    def assertNotSameFailTest expected, object
+    end
+
+    def assertNotSameWithMessageTest expected, object, message
+    end
+
+    def assertNotSameWithMessageFailTest expected, object, message
+    end
   end
 end
