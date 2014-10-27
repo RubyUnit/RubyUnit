409de7b53b31fd4fcf47bd5031d53bed028fc386
Matthew Clower <matthew.clower@wepay.com>
2014-10-26 22:29:59 -0700

    added container for test results and report

---
+++ lib/RubyUnit/Report.rb
@@ -0,0 +1,7 @@
+module RubyUnit
+  ##
+  # The RubyUnit::Report class is the test results report
+  #
+  class Report
+  end
+end
---
+++ lib/RubyUnit/Result.rb
@@ -0,0 +1,10 @@
+module RubyUnit
+  ##
+  # The RubyUnit::Result class is a result of individual tests
+  #
+  class Result
+    def initialize
+      # Automatically add result to the test results...
+    end
+  end
+end
