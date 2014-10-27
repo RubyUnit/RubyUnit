b27c9939108751821cf2d26ecc706e9719fc21d3
Matthew Clower <matthew.clower@wepay.com>
2014-10-26 23:23:22 -0700

    updating shell for test reporting

--- lib/RubyUnit/Report.rb
+++ lib/RubyUnit/Report.rb
@@ -1,7 +1,28 @@
 module RubyUnit
   ##
-  # The RubyUnit::Report class is the test results report
+  # The RubyUnit::Report module is the test results report
   #
-  class Report
-  end
+  module Report
+    @@results    = []
+    @@fail       = []
+    @@skip       = []
+    @@incomplete = []
+    @@errors     = []
+
+    def self.record result
+      @@results << result
+
+      case result.error.class
+      when AssertionFailure
+        @@fail << result
+      when SkippedTest
+        @@skip << result
+      when IncompleteTest
+        @@incomplete << result
+      else
+        if result.error.class <= Exception
+          @@errors << result
+        end
+      end
+    end
 end
--- lib/RubyUnit/Result.rb
+++ lib/RubyUnit/Result.rb
@@ -3,8 +3,26 @@ module RubyUnit
   # The RubyUnit::Result class is a result of individual tests
   #
   class Result
-    def initialize
+    attr_accessor :error
+
+    @test_case
+    @test
+    @params
+    @error
+
+    def initialize test_case, test, params = [], error = nil
+      raise TypeError "Test Case name must be a string, got #{test_case.class}" unless test_case is_a? String
+      raise TypeError "Test must be a symbol, got #{test.class}" unless test.is_a? Symbol
+      raise TypeError "Test Parameters must be an Array, got #{params.class}" unless params.is_a? Array
+      raise TypeError "Error must be an Exception or nil, got #{error.class}" unless error.nil? or error <= Exception
+
+      @test_case = test_case
+      @test      = test
+      @params    = params
+      @error     = error
+
       # Automatically add result to the test results...
+      Report.record self
     end
   end
 end
